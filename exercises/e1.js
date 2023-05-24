fe421597424a91d486e87f3106a8b1877b9b3461
Matthew Hogan <69777851+mjhogan165@users.noreply.github.com>
2023-05-23 23:21:15 -0400

    Update e1.js

--- exercises/e1.js
+++ exercises/e1.js
@@ -20,4 +20,4 @@
 // Once you're finished run the test with "npm run test-1"
 // If the test has all tests passed, switch to the next exercise file
 // If any of the tests fails, refactor the code and run the test command after you've fixed the function
-//56785678567
+//sdfasdfasfd
